76b3726f4ddae5622e4015a9068eea21d07cd045
Ilma <ilmaprova13@gmail.com>
2022-04-24 09:31:59 +0600

    connection with mongodb

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express();
@@ -6,10 +7,33 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ltwfe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        const productsCollection = client.db('dbJohn').collection('products');
+        console.log('mongo is connected')
+    }
+    finally {
+// client.close();
+    }
+}
+
+run().catch(console.dir);
+
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+
 app.get('/', (req, res) => {
     res.send('ema john is runnning');
 })
 
 app.listen(port, () => {
-    console.log('ema is running');
+    console.log('ema is running', port);
 })
